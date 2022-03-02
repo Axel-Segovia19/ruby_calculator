37f5acc63ac0921f6dc45a27baea776edc1239d7
Axel-Segovia19 <14segovia@gmail.com>
2022-03-01 23:12:27 -0700

    added a cancel

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -9,33 +9,67 @@
 # - make clear work
 
 
-def welcome
+
+def welcome1
   puts "please enter a number"
   @user_input1 = gets.strip.to_f
+welcome2
+end
+
+def welcome2
   puts "please enter an operator + - / *"
   @user_input2 = gets.strip
+  welcome3
+end
+
+def welcome3
   puts "please enter another number"
   @user_input3 = gets.strip.to_f
   puts "Please wait doing the math....."
   puts "your answer is!" 
+
   operator
 
 
 end
 
+def quit_cal
+  puts "would you like to quit? y/n"
+  user_input4 = gets.strip
+  if user_input4 == "y"
+    puts "good bye"
+    exit
+
+    elsif user_input4 == "n"
+      welcome2
+    else 
+      puts "error pick y/n"
+      quit_cal
+    end
+
+
+end
+
+
+
+
 def operator
  if @user_input2 == "+"
-  puts (@user_input1 + @user_input3)
+  @user_input1 = @user_input1 + @user_input3
+  puts @user_input1
  elsif @user_input2 == "-"
-  puts (@user_input1 - @user_input3)
+  @user_input1 = @user_input1 - @user_input3
+  puts @user_input1
  elsif @user_input2 == "*"
-  puts (@user_input1 * @user_input3)
+  @user_input1 = @user_input1 * @user_input3
+  puts @user_input1
  elsif @user_input2 == "/"
-  puts (@user_input1 / @user_input3)
+  @user_input1 = @user_input1 / @user_input3
+  puts @user_input1
  else 
   puts "error try again"
-  welcome
+  welcome1
  end
- welcome
+ quit_cal
 end
- welcome
+ welcome1
